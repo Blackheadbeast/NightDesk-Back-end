f70daf0152cfc109d2151b8845e2422b6b63aa4d
Mahad Ghazipura <mahadghazipura@Mahads-MacBook-Air.local>
2025-12-31 19:22:14 -0700

    Minimal Railway test server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import "./config.js";
 import express from "express";
 import cors from "cors";
+import routes from "./routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(routes);
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
